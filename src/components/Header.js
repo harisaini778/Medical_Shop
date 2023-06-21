8de96c2351c388823e770403d50d056d152be9c6
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-06-21 18:46:10 +0530

    Added style sheet to the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React, { useContext } from 'react';
 import { MedicineContext } from './MedicineContextProvider';
 import Cart from './Cart';
+import "./Header.css";
 
 const Header = () => {
   const { isCartOpen, toggleCart } = useContext(MedicineContext);
 
   return (
-    <header>
-      <h1>Medicine Shop</h1>
-      <button onClick={toggleCart}>{isCartOpen ? 'Close Cart' : 'Open Cart'}</button>
+    <header className="header">
+      <h1 className="header__title">Medicine Shop</h1>
+      <button className="header__cart-button" onClick={toggleCart}>
+        {isCartOpen ? 'Close Cart' : 'Open Cart'}
+      </button>
       {isCartOpen && <Cart />}
     </header>
   );
