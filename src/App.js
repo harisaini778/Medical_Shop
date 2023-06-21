65509fb203ae5ca2b22c300bcc3170a57979afe5
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-06-21 17:51:41 +0530

    Imported all the components in app components, and wrap it with the Medicine Context Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Header from './components/Header';
 
+import UserForm from './components/UserForm';
+
+import MedicineDetails from './components/MedicineDetails';
+
+import Cart from './components/Cart';
+
+import MedicineContextProvider from './components/MedicineContextProvider';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   <Header/>
+      <MedicineContextProvider>
+        <Header />
+        <UserForm />
+        <MedicineDetails />
+        <Cart/>
+    </MedicineContextProvider>
     </div>
   );
 }
