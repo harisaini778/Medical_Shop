9637fd77173d295aa2aac99c958197a635e68905
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-06-22 04:16:47 +0530

    Added style sheet for the cart, also modifies the style sheet to render on all the screen sizes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <div >
+    <div className='app'>
       <MedicineContextProvider>
         <Header />
         <UserForm />
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,26 +1,30 @@
 import React, { useContext } from 'react';
 import { MedicineContext } from './MedicineContextProvider';
+import "./Cart.css";
 
 const Cart = () => {
   const { cart, totalPrice } = useContext(MedicineContext);
 
   return (
-    <div>
-      <h2>Cart</h2>
+    <div className='container-cart'>
+    <nav className='cart-title-nav'><h2 className="cart-title">Cart</h2></nav>
+    <div className="cart">
       {cart.length === 0 ? (
-        <p>Your cart is empty</p>
+        <p className="cart-empty-message">Your cart is empty</p>
       ) : (
         <>
           {cart.map((item) => (
-            <div key={item.name}>
-              <p>{item.name}</p>
-              <p>Quantity: {item.quantity}</p>
-              <p>Price: ${item.totalPrice}</p>
+            <div key={item.name} className="cart-item">
+              <p className="cart-item-name">{item.name}</p>
+              <p className='cart-item-quantity'>{item.description }</p>
+              <p className="cart-item-quantity">Quantity: {item.quantity}</p>
+              <p className="cart-item-price">Price: ${item.totalPrice}</p>
             </div>
           ))}
-          <p>Total Price: ${totalPrice}</p>
+          <p className="cart-total">Total Price: ${totalPrice}</p>
         </>
       )}
+    </div>
     </div>
   );
 };
