7a9ebc0084eeea6efc0ec5f6fa6df4b271c82cd6
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-06-21 14:29:16 +0530

    Added Header section of the app

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Medical Shop</h1>
+            <button>Cart</button>
+   </header>
+   ); 
+}
+export default Header;
